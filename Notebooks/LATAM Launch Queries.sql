279daed8d3bf4888c8a1b1f326e795b49d08e31d
matthewvirga <matthewvirga>
2024-02-14 02:58:30 +0000

    Finished transactions view

--- Notebooks/LATAM Launch Queries.sql
+++ Notebooks/LATAM Launch Queries.sql
@@ -116,47 +116,84 @@ case
   else id
 end as event_id,
 
-id as transaction_id,
-st.event.refunded.refundReference as refund_id,
+-- id as transaction_id,
+-- invoiceid
+-- st.event.refunded.refundReference as refund_id,
 
 case
   when st.event.refunded.providerRefundReference is not null then id
-end as og_transaction_id,
+end as refunded_transaction_id,
 
 realm,
 userId,
--- created,
+created as created_date,
+st.event.occurred as event_occurred_date,
 source.type as source_type,
 source.reference as source_reference,
 
 -- amountDetails.simpleAmount.minorAmount as amountDetails_simpleAmount_minorAmount,
 -- amountDetails.simpleAmount.taxationCountryCode as amountDetails_simpleAmount_taxationCountryCode,
 
-amountDetails.amountWithTax.minorAmountInclTax as amountDetails_amountWithTax_minorAmountInclTax,
-amountDetails.amountWithTax.taxMinorAmount as amountDetails_amountWithTax_taxMinorAmount,
-amountDetails.currencyCode as amountDetails_currencyCode,
-amountDetails.amountWithTax.taxRateMinorUnits/10000 as amountDetails_amountWithTax_taxRate,
-amountdetails.amountWithTax.components[0].description as tax_description,
-amountDetails.amountWithTax.taxationCountryCode as amountDetails_amountWithTax_taxationCountryCode,
-amountDetails.amountWithTax.taxDocumentReference as amountDetails_amountWithTax_taxDocumentReference,
+case
+  when st.event.refunded.providerRefundReference is not null then st.event.refunded.amountDetails.amountWithTax.minorAmountInclTax * -1
+  when st.event.chargeback.providerChargebackReference is not null then st.event.chargeback.amountDetails.amountWithTax.minorAmountInclTax * -1
+  else amountDetails.amountWithTax.minorAmountInclTax
+end as charged_amount,
 
-st.event.occurred as event_occurred_date,
+case
+  when st.event.refunded.providerRefundReference is not null then st.event.refunded.amountDetails.currencyCode
+  when st.event.chargeback.providerChargebackReference is not null then st.event.chargeback.amountDetails.currencyCode
+  else amountDetails.currencyCode
+end as currency,
+
+case
+  when st.event.refunded.providerRefundReference is not null then st.event.refunded.amountDetails.amountWithTax.taxMinorAmount * -1
+  when st.event.chargeback.providerChargebackReference is not null then st.event.chargeback.amountDetails.amountWithTax.taxMinorAmount * -1
+  else amountDetails.amountWithTax.taxMinorAmount
+end as tax_amount,
+
+case
+  when st.event.refunded.providerRefundReference is not null then st.event.refunded.amountDetails.amountWithTax.components[0].description
+  when st.event.chargeback.providerChargebackReference is not null then st.event.chargeback.amountDetails.amountWithTax.components[0].description
+  else amountDetails.amountWithTax.components[0].description
+end as tax_description,
+
+case
+  when st.event.refunded.providerRefundReference is not null then st.event.refunded.amountDetails.amountWithTax.taxRateMinorUnits/10000
+  when st.event.chargeback.providerChargebackReference is not null then st.event.chargeback.amountDetails.amountWithTax.taxRateMinorUnits/10000
+  else amountDetails.amountWithTax.taxRateMinorUnits/10000
+end as tax_rate,
+
+case
+  when st.event.refunded.providerRefundReference is not null then st.event.refunded.amountDetails.amountWithTax.taxationCountryCode
+  when st.event.chargeback.providerChargebackReference is not null then st.event.chargeback.amountDetails.amountWithTax.taxationCountryCode
+  else amountDetails.amountWithTax.taxationCountryCode
+end as tax_country_code,
+
+
+-- amountDetails.amountWithTax.minorAmountInclTax as amountDetails_amountWithTax_minorAmountInclTax,
+-- amountDetails.amountWithTax.taxMinorAmount as amountDetails_amountWithTax_taxMinorAmount,
+-- amountDetails.currencyCode as amountDetails_currencyCode,
+-- amountDetails.amountWithTax.taxRateMinorUnits/10000 as amountDetails_amountWithTax_taxRate,
+-- amountdetails.amountWithTax.components[0].description as tax_description,
+-- amountDetails.amountWithTax.taxationCountryCode as amountDetails_amountWithTax_taxationCountryCode,
+-- amountDetails.amountWithTax.taxDocumentReference as amountDetails_amountWithTax_taxDocumentReference,
 
 case
   when st.event.pending.providerPaymentReference is not null then 'PENDING'
   when st.event.successful.providerPaymentReference is not null then 'SUCCESSFUL'
   when st.event.canceled.reason is not null then 'CANCELLED'
-  when (st.event.failed.reason is not null OR st.event.failed.providerPaymentReference is not null) then 'FAILED'
-  when (st.event.retrying.nextretry is not null OR st.event.retrying.reason is not null) then 'RETRYING'
+  when st.event.failed.providerPaymentReference is not null then 'FAILED'
+  when st.event.retrying.nextretry is not null then 'RETRYING'
   when st.event.refunding.providerRefundReference is not null then 'REFUNDING'
   when st.event.refunded.providerRefundReference is not null then 'REFUNDED'
   when st.event.refundFailed.reason is not null then 'REFUND FAILED'
   when st.event.chargeback.providerChargebackReference is not null then 'CHARGEBACK'
   when st.event.timedOut.emptyObject is not null then 'TIMED OUT'
   when st.event.revoked.reason is not null then 'REVOKED'
-  when (st.event.chargebackRejected.providerChargebackReference is not null OR st.event.chargebackRejected.reason is not null) then 'CHARGEBACK REJECTED'
+  when st.event.chargebackRejected.providerChargebackReference is not null then 'CHARGEBACK REJECTED'
   when (st.event.corrected.providerPaymentReference is not null OR st.event.corrected.providerRefundReference is not null) then 'CORRECTED'
-end as event_state_type,
+end as event_type,
 
 case
   when st.event.pending.providerPaymentReference is not null then st.event.pending.providerPaymentReference
@@ -185,23 +222,29 @@ case
   when st.event.refunded.providerRefundReference is not null then st.event.refunded.source
 end as refund_source,
 
-st.event.retrying.nextretry as next_retry_date,
+replace(provider,'PAYMENT_PROVIDER_','') as payment_provider,
+-- billingAddressId,
 
-provider,
-billingAddressId,
-type,
+-- case
+--   when type = 'PAYMENT_TRANSACTION_TYPE_INTERACTIVE' 
+--     and (amountDetails.amountWithTax.minorAmountInclTax = 0 
+--           OR amountDetails.amountWithTax.minorAmountInclTax is null) 
+--     then 'FIRST_FREE'
+--   when type = 'PAYMENT_TRANSACTION_TYPE_INTERACTIVE' and st.event.successful.providerPaymentReference is not null then 'FIRST'
+--   when (st.event.refunding.providerRefundReference is not null 
+--           OR st.event.refunded.providerRefundReference is not null) 
+--     then 'REFUND'
+--   when type = 'PAYMENT_TRANSACTION_TYPE_AUTOMATED' then 'RECURRING'
+-- end as transaction_type,
+
+replace(type, 'PAYMENT_TRANSACTION_TYPE_','') as payment_type,
 merchantAccount,
-items[0].subscriptionDetails.serviceperiod.startdate as serviceperiod_startdate,
-items[0].subscriptionDetails.serviceperiod.enddate as serviceperiod_enddate,
-invoiceid
+items[0].subscriptionDetails.serviceperiod.startdate as service_period_startdate,
+items[0].subscriptionDetails.serviceperiod.enddate as service_period_enddate,
+st.event.retrying.nextretry as next_retry_date
 
 from bolt_finint_prod.latam_silver.fi_transactionv2_enriched
 LATERAL VIEW explode(stateTransitions) st as event
 group by all
 
 )
-
--- COMMAND ----------
-
-select * from Transactions
-where userid = 'USERID:bolt:2e5f7ca9-d311-40ad-9bd1-28e561709f50'
