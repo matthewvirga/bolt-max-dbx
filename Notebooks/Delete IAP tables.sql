f1d49451389e2791c47c6dd776e877e7a4ba64a9
matthewvirga <matthewvirga>
2024-09-03 18:51:33 +0000

    Enhanced Autoloader Script
    added metadata, added path pattern to select which file, added trigger once to run and stop dynamically

---
+++ Notebooks/Delete IAP tables.sql
@@ -0,0 +1,14 @@
+-- Databricks notebook source
+Drop table if exists bolt_finint_int.bronze.apple_reports_s3
+
+-- COMMAND ----------
+
+Drop table if exists bolt_finint_int.bronze.amazon_reports_s3
+
+-- COMMAND ----------
+
+Drop table if exists bolt_finint_int.bronze.roku_reports_s3
+
+-- COMMAND ----------
+
+Drop table if exists bolt_finint_int.bronze.google_reports_s3
--- Notebooks/autoloader s3 test.py
+++ Notebooks/autoloader s3 test.py
@@ -1,28 +1,37 @@
 # Databricks notebook source
-# DBTITLE 1,Autoloader Script with parameters
+from pyspark.sql.functions import current_timestamp, input_file_name
+
 # Access parameters passed from the Workflow UI
 s3_bucket_path = dbutils.widgets.get("s3_bucket_path")
 table_name = dbutils.widgets.get("table_name")
 schema_location = dbutils.widgets.get("schema_location")
 checkpoint_location = dbutils.widgets.get("checkpoint_location")
+path_pattern = dbutils.widgets.get("path_pattern") # This is to determine which report to grab sales, finance, earnings etc.
 
-# Read from S3 bucket using Auto Loader
+# Read from S3 bucket using Auto Loader, filtering for files based on the path pattern
 df = (spark.readStream
-            .format("cloudFiles")
-            .option("cloudFiles.format", "csv")
-            .option("header", "true") 
-            .option("inferSchema", "true")
-            .option("cloudFiles.schemaLocation", schema_location)
-            .load(s3_bucket_path))
+    .format("cloudFiles")
+    .option("cloudFiles.format", "csv")
+    .option("header", "true") 
+    .option("inferSchema", "true")
+    .option("cloudFiles.schemaLocation", schema_location)
+    .option("cloudFiles.includePathPatterns", path_pattern)  # Use the path pattern from the widget
+    .option("cloudFiles.pathGlobFilter", path_pattern)  # Apply path pattern as glob filter for case sensitivity
+    .load(s3_bucket_path))
 
 # Modify column names in the schema
 new_column_names = [col.replace(' ', '_') for col in df.schema.names]
 df = df.toDF(*new_column_names)
 
+# Add metadata columns
+df_with_metadata = df.withColumn("appended_timestamp", current_timestamp()) \
+                    .withColumn("file_name", input_file_name())
+
 # Write to Delta table
-(df.writeStream
+streaming_query = (df_with_metadata.writeStream
     .format("delta")
     .option("checkpointLocation", checkpoint_location)
     .option("mergeSchema", "true")
     .outputMode("append")
-    .table(table_name))
+    .trigger(once=True)  # Trigger the job only once
+    .start(table_name))
